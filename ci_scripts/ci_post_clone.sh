33b258ea69fd2e230e155274857bbde29b501c37
Abron Studio <teamabron@gmail.com>
2025-10-16 18:30:20 +0330

    chore: update code signing style to Automatic and upgrade SwiftUI-Flow package to version 3.1.0; enhance CI script to install CocoaPods dependencies

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -5,6 +5,12 @@
 # allow using macros
 defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
 
+# Install CocoaPods using Homebrew.
+brew install cocoapods
+
+# Install dependencies you manage with CocoaPods.
+SKIP_PRE_INSTALL=1 pod install
+
 # resolve packages
 cd ..
 xcodebuild -resolvePackageDependencies
